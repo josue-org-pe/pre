069c85acf45d3abb13113ecf4d7bd8f1e467687a
josue-org-pe <josue.monjemaron@gmail.com>
2025-06-27 23:32:02 -0500

    2doerror provocado st.button

--- INICIO.py
+++ INICIO.py
@@ -8,7 +8,10 @@ sentry_sdk.init(
 st.set_page_config(page_title="PLANETA ROJO🪐", page_icon="👽", layout="wide")
 #definicion de VARIABLES 
 
-#universidades = ['UNI', 'UNMSM', 'UNAP']
+if st.button("Provocar error mínimo"):
+    raise ValueError("Este es un error de prueba para Sentry")
+
+universidades = ['UNI', 'UNMSM', 'UNAP']
 
 facultades_uni = ['FIIS','FC','FAUA', 'FIC', 'FIEECS', 'FIGMM','FIEE','FIM','FIQT','FIP','FIA']
 facultades_unmsm = ["FM", "FFB", "FO", "FP", "FCS", "FDCP", "FCA", "FCC", "FCE", "FLCH", "FE", "FQIQ", "FCF", "FCB", "FCM", "FIGMMG", "FII", "FIEE", "FISI", "FMV"]
